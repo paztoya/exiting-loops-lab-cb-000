a08d99c4570108dd39465449afd1949dfb0c4c69
Daniel Paz <daniel@paztoya.com>
2020-01-20 19:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function breakOut(array, changeValue, stopValue){
   array.forEach(changeValue)
   for (let i=0; i < 0; i++){
     if (changeValue == stopValue){
-
+      break;
     }
     return array;
   }
